ccd3f8edd5a44879b7580d4fa793dcfd90b5f6ba
ndkmath1 <ndkmath1@gmail.com>
2017-11-16 01:12:46 +0700

    add product status, product sell address activity

---
+++ app/src/main/java/com/coho/moki/ui/product/ProductSellAddressActivity.java
@@ -0,0 +1,26 @@
+package com.coho.moki.ui.product;
+
+import com.coho.moki.ui.base.BaseActivity;
+
+/**
+ * Created by Khanh Nguyen on 11/16/2017.
+ */
+
+public class ProductSellAddressActivity extends BaseActivity {
+
+    @Override
+    public int setContentViewId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+}
